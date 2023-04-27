41a70dd96b1717d572594ba8fc651879218ac697
MagomedBatalov <77672196+MagomedBatalov@users.noreply.github.com>
2023-04-27 18:26:56 +0300

    Update FindMin.cpp

--- FindMin.cpp
+++ FindMin.cpp
@@ -6,39 +6,40 @@ using namespace std;
 
 // Объявление шаблона, В данном случае шаблонной функции. Функция подстраивается и изменякется в зависимости от того какой тип данных в нее входит. 
 
-//функция копирования одного массива в другой
+//Шаблонная функция копирования одного массива в другой
 template<typename T>
 void copy_array(const T* from, T* to, size_t count) { // 
-    for (size_t i = 0; i < count; i++) {
-        to[i] = from[i];
-    }
+	for (size_t i = 0; i < count; i++) {
+		to[i] = from[i];
+	}
 }
 
 // функци вывода массива
 template<typename T>
 void print_array(const T* parr, size_t count) {
-    for (size_t i = 0; i < count; i++) {
-        cout << parr[i] << " ";
-    }
-    cout << endl;
+	for (size_t i = 0; i < count; i++) {
+		cout << parr[i] << " ";
+	}
+	cout << endl;
 }
 
 //поиск индекса, принадлежащему минимальному элементу массива
 template<typename T>
-size_t findmin(const T* parr, size_t count) {
-    size_t min = 0;
-    if (count == 0) {
-        return o;
-    }
-    T elem = parr[0];
-    for (size_t i = 0; i < count; i++) {
-        if (parr[i] < elem) {
-            elem = parr[i];
-            min = i;
-        };
-    };
-    return min
+int findmin(const T* parr, size_t count) {
+	size_t min = 0;
+	if (count == 0) {
+		return 0;
+	}
+	T elem = parr[0];
+	for (size_t i = 0; i < count; i++) {
+		if (parr[i] < elem) {
+			elem = parr[i];
+			min = i;
+		};
+	};
+	return min;
 }
+
 
 int main(int argc, char** argv) {
     int arr[20], arr2[20];
