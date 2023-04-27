fe9fbb833fb6ec887e746cf3c5d97b0308bd2632
MagomedBatalov <77672196+MagomedBatalov@users.noreply.github.com>
2023-04-27 19:18:26 +0300

    Update sumVec.cpp

--- sumVec.cpp
+++ sumVec.cpp
@@ -7,73 +7,72 @@ using namespace std;
 //Шаблонная функция, выводящая массив на экран
 template<typename T>
 void PrintVector(const vector<T>& v) {
-	for (const auto& item : v) {
-		cout << item << " ";
-	}
-	cout << endl;
+    for (const auto& item : v) {
+        cout << item << " ";
+    }
+    cout << endl;
 }
 
-//Суммирует матрицу 
+//Функция, суммирующая вектор векторов 
 vector<int> sum(const vector<vector<int>>& v) {
-	vector<int> result;
-	for (const auto& item : v) {
-		result.push_back(accumulate(item.begin(), item.end(), 0));
-	}
-	return result;
+    vector<int> result;
+    for (const auto& item : v) {
+        result.push_back(accumulate(item.begin(), item.end(), 0)); //запушить в конец(сумма(вектор.начало, вектор.конец, начальная сумма))
+    }
+    return result;
 }
 
-//1a
+//шаблонная функиця суммы вектора векторов
 template<typename T>
 vector<T> sum(const vector<vector<T>>& v) {
-	vector<T> result;
-	for (const auto& item : v) {
-		result.push_back(accumulate(item.begin(), item.end(), 0));
-	}
-	return result;
+    vector<T> result;
+    for (const auto& item : v) {
+        result.push_back(accumulate(item.begin(), item.end(), 0));
+    }
+    return result;
 }
 
-template<typename <T>>
+template<typename T>
 class MatrixReduceOperation {
-protected: 
-	vector<vector<T>> matrix:
-public: 
-	MatrixReduceOperation() {}
+protected:
+    vector<vector<T>> matrix;
+public:
+    MatrixReduceOperation() {}
 
-	MatrixReduceOperation(const vector<vector<T>>& v) {
-		matrix = v;
-	}
+    MatrixReduceOperation(const vector<vector<T>>& v) {
+        matrix = v;
+    }
 
-	virtual vector<T> calc() {
-		vector<T> result;
-		for (const auto& item : matrix){
-			result.push_back(accumulate(item.begin(), item.end(), 0));
-
-		}
-		return result;
-	}
+    virtual vector<T> calc() {
+        vector<T> result;
+        for (const auto& item : matrix){
+            result.push_back(accumulate(item.begin(), item.end(), 0));
+        }
+        return result;
+    }
 };
 
-template<typename <T1>>
-class MatrixRowSum : virtual public MatrixReduceOperation<T1> {
-public: 
-	MatrixRowSum() : MatrixReduceOperation<T1>() {}
-	MatrixRowSum(const vector<vector<T1>>& v) : MatrixReduceOperation<T1>(v) {}
-	vector<T1> calc() {
-		for (const auto& item : MatrixReduceOperation<T1>::matrix) {
-			result.push_back(accumulate(item.begin(), item.end(), 0));
-		}
-		return result;
-	}
+template<typename T>
+class MatrixRowSum : virtual public MatrixReduceOperation<T> {
+public:
+    MatrixRowSum() : MatrixReduceOperation<T>() {}
+    MatrixRowSum(const vector<vector<T>>& v) : MatrixReduceOperation<T>(v) {}
+    vector<T> calc() {
+        vector<T> result;
+        for (const auto& item : MatrixReduceOperation<T>::matrix) {
+            result.push_back(accumulate(item.begin(), item.end(), 0));
+        }
+        return result;
+    }
 };
 
 int main() {
-	vector<vector<int>> v = { {1, 2, 3, 6, 4, 5} {6, 4, 3, 21,} {5, 6, 2, 1} }
-	vector<int> m = sum(v);
-	PrintVector(m);
+    vector<vector<int>> v = { {1, 2, 3, 6, 4, 5}, {6, 4, 3, 21}, {5, 6, 2, 1} };
+    vector<int> m = sum(v);
+    PrintVector(m);
 
-	MatrixReduceOperation k = v;
-	PrintVector(k.calc());
+    MatrixReduceOperation<int> k(v);
+    PrintVector(k.calc());
 
-
-
+    return 0;
 }
